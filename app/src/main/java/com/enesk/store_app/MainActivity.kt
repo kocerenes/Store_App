ba578e2ca56d5332fdb7cf7834618f906f905a6c
kocerenes <eneskocerr41@gmail.com>
2022-07-26 16:00:00 +0300

    Initial commit

---
+++ app/src/main/java/com/enesk/store_app/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.enesk.store_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.enesk.store_app.presentation.ui.theme.Store_AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Store_AppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Store_AppTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/enesk/store_app/presentation/ui/theme/Shape.kt
@@ -0,0 +1,10 @@
+package com.enesk.store_app.presentation.ui.theme
+
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
